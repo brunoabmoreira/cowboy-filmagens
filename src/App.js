4841e2d477d030d19adb9bef6044673e69e43f95
brunoabmoreira <52184560+brunoabmoreira@users.noreply.github.com>
2022-02-14 15:33:24 -0300

    modified:   src/App.css 	modified:   src/App.js 	modified:   src/components/Conteudo.css 	modified:   src/components/Conteudo.js 	new file:   src/imagens/AnivIsabelVovoElza.png 	new file:   src/imagens/afonso53.fw.png 	new file:   src/imagens/botao.fw.png 	new file:   src/imagens/cantandonorio.fw.png 	new file:   src/imagens/fazendaeicapui.fw.png 	new file:   src/imagens/natalmoreira.fw.png

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import Cabecalho from './components/Cabecalho'
 import Fita from './components/Fita'
 import './App.css';
 import Conteudo from './components/Conteudo'
-
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
-        <Cabecalho />
-        <Fita />
-        <Conteudo />
+          <Cabecalho />
+          <Fita />
+          <Conteudo />
     </div>
   );
 }
--- src/components/Conteudo.js
+++ src/components/Conteudo.js
@@ -1,13 +1,19 @@
 import style from './Conteudo.css'
 import saojoao2009 from '../imagens/saojoao2009.fw.png'
+import AnivIsabelVovoElza from '../imagens/AnivIsabelVovoElza.png'
+import botao from '../imagens/botao.fw.png'
+import afonso53 from '../imagens/afonso53.fw.png'
+import fazendaeicapui from '../imagens/fazendaeicapui.fw.png'
+import natalmoreira from '../imagens/natalmoreira.fw.png'
+import cantandonorio from '../imagens/cantandonorio.fw.png'
 
 function Conteudo() {
     return (
         <div id="conteudo">
             <div id='screen'>
                 <div id='foto'>
-                    <a href="http://www.google.com" target='_blank' >
-                        <img src={saojoao2009} alt='teste' id='fotos' />
+                    <a href="https://youtu.be/X8oVCB2fRFk" target={'_blank'} rel="noopener noreferrer" >
+                        <img src={saojoao2009} alt='saojoao2009' id='fotos' />
                     </a>
                 </div>
                 <div id='texto'>
@@ -22,26 +28,119 @@ function Conteudo() {
                         <strong>Um resumo:</strong> Aproveitando a boa hospitalidade de sempre a família resolve passar o São João do ano de 2009 no sítio São Jerônimo do amigo Boanerges. Momentos felizes registrados e eternizados nessa filmagem. <br></br><br></br>
                         Rodas de conversas, violão, quadrilha e muito mais.
                     </p>
+                    <a href='https://youtu.be/X8oVCB2fRFk' target={'_blank'} rel="noopener noreferrer">
+                        <img id='botao' alt='botao' src={botao}></img>
+                    </a>
                 </div>
             </div>
             <div id='screen'>
                 <div id='foto'>
-                    <a href="http://www.google.com" target='_blank' >
-                        <img src={saojoao2009} alt='teste' id='fotos' />
+                    <a href="https://youtu.be/QkyfXrNpKCU" target={'_blank'} rel="noopener noreferrer">
+                        <img src={AnivIsabelVovoElza} alt='AnivIsabelVovoElza' id='fotos' />
                     </a>
                 </div>
                 <div id='texto'>
-                    <h3>testando textos</h3>
+                    <h3>ANIVERSÁRIO ISABEL</h3>
+                    <p>
+                        <strong>Ano da Filmagem:</strong> Desconhecido
+                    </p>
+                    <p>
+                        <strong>Participações:</strong> Elza, Isabel, Afonso, Moreira, Venina, Rodrigo, Bruno, Victor, Monalisa. 
+                    </p>
+                    <p>
+                        <strong>Um resumo:</strong> Roda de conversas com dona Elza Lopes Moreira, durante a comemoração do aniversário de Isabel.
+                    </p>
+                    <a href='https://youtu.be/QkyfXrNpKCU' target={'_blank'} rel="noopener noreferrer">
+                        <img id='botao' src={botao} alt='botao'></img>
+                    </a>
                 </div>
             </div>
             <div id='screen'>
                 <div id='foto'>
-                    <a href="http://www.google.com" target='_blank' >
-                        <img src={saojoao2009} alt='teste' id='fotos' />
+                    <a href="https://youtu.be/lV3eeQzXpZI" target={'_blank'} rel="noopener noreferrer">
+                        <img src={afonso53} alt='afonso53' id='fotos' />
                     </a>
                 </div>
                 <div id='texto'>
-                    <h3>testando textos</h3>
+                    <h3>ANIVERSÁRIO 53 ANOS - AFONSO</h3>
+                    <p>
+                        <strong>Ano da Filmagem:</strong> 1998
+                    </p>
+                    <p>
+                        <strong>Participações:</strong> Afonso, Isabel, Santina, Venina, Moreira, Francisco, Jefferson, Reinaldo, Rodrigo, Rebeka, Bruno, Monalisa, Emilia, Lucimar, Tereza, Victor e muitos outros amigos.  
+                    </p>
+                    <p>
+                        <strong>Um resumo:</strong> Comemoração do aniversário do Afonso com muito forró.
+                    </p>
+                    <a href='https://youtu.be/lV3eeQzXpZI' target={'_blank'} rel="noopener noreferrer">
+                        <img id='botao' alt='botao' src={botao}></img>
+                    </a>
+                </div>
+            </div>
+            <div id='screen'>
+                <div id='foto'>
+                    <a href="https://youtu.be/SLxIqYU0jfk" target={'_blank'} rel="noopener noreferrer">
+                        <img src={fazendaeicapui} alt='teste' id='fotos' />
+                    </a>
+                </div>
+                <div id='texto'>
+                    <h3>VISITA À FAZENDA E À ICAPUÍ</h3>
+                    <p>
+                        <strong>Ano da Filmagem:</strong> 2012
+                    </p>
+                    <p>
+                        <strong>Participações:</strong> 
+                    </p>
+                    <p>
+                        <strong>Um resumo:</strong>
+                    </p>
+                    <a href='https://youtu.be/SLxIqYU0jfk' target={'_blank'} rel="noopener noreferrer">
+                        <img id='botao' alt='botao' src={botao}></img>
+                    </a>
+                </div>
+            </div>
+            <div id='screen'>
+                <div id='foto'>
+                    <a href="https://youtu.be/8BB1v5w4BVg" target={'_blank'} rel="noopener noreferrer">
+                        <img src={natalmoreira} alt='teste' id='fotos' />
+                    </a>
+                </div>
+                <div id='texto'>
+                    <h3>NATAL FAMÍLIA MOREIRA</h3>
+                    <p>
+                        <strong>Ano da Filmagem:</strong>
+                    </p>
+                    <p>
+                        <strong>Participações:</strong> 
+                    </p>
+                    <p>
+                        <strong>Um resumo:</strong>
+                    </p>
+                    <a href='https://youtu.be/8BB1v5w4BVg' target={'_blank'} rel="noopener noreferrer">
+                        <img id='botao' alt='botao' src={botao}></img>
+                    </a>
+                </div>
+            </div>
+            <div id='screen'>
+                <div id='foto'>
+                    <a href="https://youtu.be/hvgd8Xo-C9U" target={'_blank'} rel="noopener noreferrer">
+                        <img src={cantandonorio} alt='teste' id='fotos' />
+                    </a>
+                </div>
+                <div id='texto'>
+                    <h3>AFONSO CANTANDO NO RIO</h3>
+                    <p>
+                        <strong>Ano da Filmagem:</strong>
+                    </p>
+                    <p>
+                        <strong>Participações:</strong> 
+                    </p>
+                    <p>
+                        <strong>Um resumo:</strong>
+                    </p>
+                    <a href='https://youtu.be/hvgd8Xo-C9U' target={'_blank'} rel="noopener noreferrer">
+                        <img id='botao' alt='botao' src={botao}></img>
+                    </a>
                 </div>
             </div>
         </div>
